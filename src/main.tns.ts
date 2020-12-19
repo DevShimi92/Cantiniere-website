c94b37c4ed697a91a1fc63948bd2c58c4fa39607
DevShimi92 <hospitalier.philippe@gmail.com>
2020-12-19 17:42:27 +0100

    Mobile update
    add more icon for menu in menu ,
    added something for work in dark mode and light mode (with dark mode on) and update css

--- src/main.tns.ts
+++ src/main.tns.ts
@@ -3,6 +3,23 @@ import { platformNativeScriptDynamic } from '@nativescript/angular';
 
 import { AppModule } from '@src/app/app.module';
 
+/*import { android, on, launchEvent, ApplicationEventData } from '@nativescript/core/application';
+
+// Typescript will require you to define those types  [WHITE THEME FORCED IN DARK THEME]
+declare namespace androidx {
+ export namespace appcompat {
+  export namespace app {
+   export const AppCompatDelegate: any;
+  }
+ }
+}
+
+if (android) {
+ on(launchEvent, (args: ApplicationEventData) => {
+  androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode(androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO)
+ });
+}*/
+
 // A traditional NativeScript application starts by initializing global objects,
 // setting up global CSS rules, creating, and navigating to the main page.
 // Angular applications need to take care of their own initialization: modules, components, directives, routes, DI providers.
