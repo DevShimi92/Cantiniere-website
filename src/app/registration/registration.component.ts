bd30936fb222472273c2a6150cd6073cab655edf
DevShimi92 <hospitalier.philippe@gmail.com>
2021-02-16 15:59:55 +0100

    Added form for create account (witout service) on desktop view

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,20 +1,67 @@
-  
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent implements OnInit {
-  title = 'Cantiniere-website';
 
-  constructor() {
-   // do nothing.
- }
+export class RegistrationComponent implements OnInit {
+  
+  registrationForm: FormGroup;
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
-    // do nothing.
+    
+    this.registrationForm = this.formBuilder.group({
+      lastName:'',
+      firstNName:'',
+      email: '',
+      password: '',
+      checkPassword: ''
+    });
+
+  }
+
+  onSubmit():void {
+
+    if(!this.registrationForm.value.lastName)
+      {
+        console.log("nom vide")
+      }
+    if(!this.registrationForm.value.firstNName)
+      {
+        console.log("prénom vide")
+      }
+    if(!this.registrationForm.value.email)
+      {
+        console.log("email vide")
+      }
+    else if (!this.validateEmail(this.registrationForm.value.email))
+      {
+        console.log("email invalide")
+      }
+    if(!this.registrationForm.value.password )
+      {
+        console.log("Mot de passe 1 vide")
+      }
+    else if (!this.registrationForm.value.checkPassword)
+      {
+        console.log("Mot de passe 2 vide")
+      }
+    else if(this.registrationForm.value.password != this.registrationForm.value.checkPassword)
+      {
+        console.log("Mot de passe différent")
+      }
+    
+  }
+
+
+  validateEmail(email:string):boolean {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
   }
 
 }
